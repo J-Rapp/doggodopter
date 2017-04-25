42f528cfe3c8461819f3e8d05604840266d6d371
Jacob Rapp <jake.rapp322@gmail.com>
2017-04-25 08:42:25 -0700

    Adds LoginForm object to help with session validation

---
+++ app/models/login_form.rb
@@ -0,0 +1,23 @@
+class LoginForm
+  include ActiveModel::Model
+
+  attr_accessor :username, :password
+
+  validates_presence_of :username, allow_blank: false, message: 'Enter your username.'
+  validates_presence_of :password, allow_blank: false, message: 'Enter your password.'
+
+  validate :authentic_user
+
+  def authentic_user
+    @user = User.find_by(username: @username)
+    if @user.nil?
+      @errors.add('username and password', 'were entered incorrectly')
+    elsif @user.authenticate(@password) == false
+      @errors.add('username and password', 'were entered incorrectly')
+    end
+  end
+
+  def user_id
+    @user.id || User.find_by(username: @username).id
+  end
+end
