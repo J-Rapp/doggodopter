b9cbfeff04db36247ea8902bfec60256f3edd111
Jacob Rapp <jake.rapp322@gmail.com>
2017-04-25 08:34:42 -0700

    Builds out sessions controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     return if object.errors.messages[form_field].blank?
     object.errors.messages[form_field].join(', ')
   end
+
+  def user_session(user_id)
+    session[:user_id] = user_id
+  end
+
+  def end_user_session
+    session[:user_id] = nil
+  end
 end
