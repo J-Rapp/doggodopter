740a73c02f0dd25483db37483b1fb12be7f9c93b
Jacob Rapp <jake.rapp322@gmail.com>
2017-04-25 14:10:25 -0700

    Renames username column to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  validates :username, uniqueness: { message: 'Username already taken' },
-                       presence: { allow_blank: false, message: 'Enter a username.' }
+  validates :Email, uniqueness: { message: 'Email already taken.' },
+                    presence: { allow_blank: false, message: 'Enter an email.' }
   validates :password, length: { minimum: 6 }
 
   has_secure_password
