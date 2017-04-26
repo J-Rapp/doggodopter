9bc5bdc5c21657aefa58a8bdd53c86a570e27e89
Jacob Rapp <jake.rapp322@gmail.com>
2017-04-26 01:19:35 -0700

    Able to make appropriate AJX call

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -7,17 +7,31 @@ $(document).ready(() => {
   Snuffle.handleMatch();
 });
 
+Snuffle.getMetaToken = function() {
+  let metas = document.getElementsByTagName('meta');
+  for (let i=0; i<metas.length; i++) {
+    if (metas[i].getAttribute('name') === 'csrf-token') {
+      return metas[i].getAttribute('content');
+    }
+  }
+}
+
 Snuffle.handleIgnore = function() {
   $('.ignore').click((event) => {
+
     const dogCard = $(event.target).parents('.dog-card'),
           dogId = dogCard.data('dog'),
           userId = dogCard.data('user')
 
     $.ajax({
-      url: '/match',
+      url: 'users/' + userId + '/matches',
       data: {
-        user_id: userId,
-        dog_id: dogId
+        authenticity_token: Snuffle.getMetaToken(),
+        match: {
+          user_id: userId,
+          dog_id: dogId,
+          selected: false
+        }
       },
       type: 'POST',
       dataType: 'json',
@@ -29,7 +43,9 @@ Snuffle.handleIgnore = function() {
       error: (xhr, response) => {
         console.log('whoops')
         // card remains in place
-    });
+      }
+    })
+
   });
 };
 
@@ -39,23 +55,29 @@ Snuffle.handleMatch = function() {
           dogId = dogCard.data('dog'),
           userId = dogCard.data('user')
 
-    // $.ajax({
-    //   url: '/match',
-    //   data: {
-    //     user_id: userId,
-    //     dog_id: 
-    //   },
-    //   type: 'POST',
-    //   dataType: 'json',
-    //   success: (response) => {
-    //     console.log('we have lift off')
-    //     // card animates left and red
-    //     // next card is brought forward
-    //   },
-    //   error: (xhr, response) => {
-    //     console.log('whoops')
-    //     // card remains in place
-    // });
+    $.ajax({
+      url: 'users/' + userId + '/matches',
+      data: {
+        authenticity_token: Snuffle.getMetaToken(),
+        match: {
+          user_id: userId,
+          dog_id: dogId,
+          selected: true
+        }
+      },
+      type: 'POST',
+      dataType: 'json',
+      success: (response) => {
+        console.log('we have lift off')
+        // card animates right and green
+        // next card is brought forward
+      },
+      error: (xhr, response) => {
+        console.log('whoops')
+        // card remains in place
+      }
+    })
+
   });
 }
 
--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,11 +1,12 @@
 class MatchesController < ApplicationController
   def create
+    p params
     @match = Match.create(match_params)
   end
 
   private
 
   def match_params
-    params.require(:match).permit(:selected)
+    params.require(:match).permit(:selected, :user_id, :dog_id)
   end
 end
